eb9aa752a55d2a94bcee02cce29acaffc853a2ed
Charlotte Wilson <charlottead@live.co.uk>
2017-08-18 08:33:34 +0100

    gitignore for cache files and update scheduling

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 "# redditcritiquethreadbot" 
+
+A bot for sending a private message whenever there is a new writing critique thread on r/writing
+
+Simply use this then run as a windows service to automate. Or, using cron with linux.
--- writing_notification_bot.py
+++ writing_notification_bot.py
@@ -10,14 +10,20 @@ https://github.com/CAWilson94/redditcritiquethreadbot
 
 import praw
 import re
+import constants
 
 reddit = praw.Reddit('botwriting')
 
 subreddit = reddit.subreddit('writing')
 
-for submission in subreddit.hot(limit=5):
-	if re.search("Weekly Critique Thread", submission.title, re.IGNORECASE):
+already_done = []
+
+for submission in subreddit.new(limit=5):
+	if re.search("writing", submission.title, re.IGNORECASE):
 		print("Title: ", submission.title)
 		print("Text: ", submission.selftext)
 		print("Score: ", submission.score)
 		print("---------------------------------\n")
+		if submission.id not in already_done:
+			reddit.redditor(constants.USER_NAME).message('WRITING POST', "NEW WRITING THREAD: GO POST!")
+			already_done.append(submission.id)
