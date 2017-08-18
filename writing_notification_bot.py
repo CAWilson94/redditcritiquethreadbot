8503cedf6fd6503d278df3492e928f9572ad33e9
Charlotte Wilson <charlottead@live.co.uk>
2017-08-18 07:56:45 +0100

    find weekly critique thread from hot and print

--- writing_notification_bot.py
+++ writing_notification_bot.py
@@ -9,13 +9,15 @@ https://github.com/CAWilson94/redditcritiquethreadbot
 '''
 
 import praw
+import re
 
 reddit = praw.Reddit('botwriting')
 
 subreddit = reddit.subreddit('writing')
 
 for submission in subreddit.hot(limit=5):
-    print("Title: ", submission.title)
-    print("Text: ", submission.selftext)
-    print("Score: ", submission.score)
-    print("---------------------------------\n")
+	if re.search("Weekly Critique Thread", submission.title, re.IGNORECASE):
+		print("Title: ", submission.title)
+		print("Text: ", submission.selftext)
+		print("Score: ", submission.score)
+		print("---------------------------------\n")
